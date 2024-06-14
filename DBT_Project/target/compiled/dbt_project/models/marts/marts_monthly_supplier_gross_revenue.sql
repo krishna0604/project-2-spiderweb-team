790f5dd940ca8a1db3969d56c0ba6e7b5660f075
krishna0604 <124908447+krishna0604@users.noreply.github.com>
2024-06-14 15:42:13 +0300

    Add files via upload

---
+++ DBT_Project/target/compiled/dbt_project/models/marts/marts_monthly_supplier_gross_revenue.sql
@@ -0,0 +1,29 @@
+
+
+WITH supplier_revenue AS (
+    SELECT
+        EXTRACT(MONTH FROM o.orderDate) AS month,
+        EXTRACT(YEAR FROM o.orderDate) AS year,
+        s.companyName AS supplier_name,
+        SUM((od.unitPrice * (1 - od.discount)) * od.quantity) AS gross_revenue
+    FROM
+        "postgres"."public"."staging_orders" o
+    JOIN "postgres"."public"."staging_order_details" od ON o.orderID = od.orderID
+    JOIN "postgres"."public"."staging_products" p ON od.productID = p.productID
+    JOIN "postgres"."public"."staging_suppliers" s ON p.supplierID = s.supplierID
+    GROUP BY
+        EXTRACT(MONTH FROM o.orderDate),
+        EXTRACT(YEAR FROM o.orderDate),
+        s.companyName
+)
+SELECT
+    month,
+    year,
+    supplier_name,
+    gross_revenue
+FROM
+    supplier_revenue
+ORDER BY
+    year,
+    month,
+    gross_revenue DESC
