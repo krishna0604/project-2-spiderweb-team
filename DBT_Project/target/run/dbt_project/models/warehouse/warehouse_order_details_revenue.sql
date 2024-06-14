84ae8f3bd244711cbaa04f227c71ce689c095a9c
krishna0604 <124908447+krishna0604@users.noreply.github.com>
2024-06-14 15:48:23 +0300

    Add files via upload

---
+++ DBT_Project/target/run/dbt_project/models/warehouse/warehouse_order_details_revenue.sql
@@ -0,0 +1,28 @@
+
+  
+    
+
+  create  table "postgres"."public"."warehouse_order_details_revenue__dbt_tmp"
+  
+  
+    as
+  
+  (
+    
+
+WITH order_details AS (
+    SELECT * FROM "postgres"."public"."staging_order_details"
+),
+order_revenue AS (
+    SELECT
+        orderid,
+        productid,
+        unitprice,
+        quantity,
+        discount,
+        (unitprice * quantity * (1 - discount)) AS revenue
+    FROM order_details
+)
+SELECT * FROM order_revenue
+  );
+  
